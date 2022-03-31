d806244af672b631fe38849a4c7729ebfcdabf17
dioreyhans <58151425+dioreyhans@users.noreply.github.com>
2022-03-31 08:47:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+const updateScreen = (number)=>{
+    calculatorScreen.value = number
+}
+
+const numbers= document.querySelectorAll(".number")
+const operators= document.querySelectorAll(".operator")
+const equalSign= document.querySelector(".equal-sign")
+const clearBtn= document.querySelector(".all-clear")
+const decimal= document.querySelector(".decimal")
+const percentage= document.querySelector(".percentage")
+
+let prevNumber ="";
+let calculationOperator="";
+let currentNumber="0";
+
+const inputNumber= (number)=>{
+    if (currentNumber==="0") {
+        currentNumber =number
+    } else {
+        currentNumber +=number
+    }   
+}
+const inputOperator= (operator)=>{
+    if(calculationOperator===""){
+        prevNumber=currentNumber
+    }
+    calculationOperator = operator
+    currentNumber = ''
+}
+
+numbers.forEach((number)=>{
+    number.addEventListener("click", (event)=>{
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+operators.forEach((operator)=>{
+    operator.addEventListener("click", (event)=>{
+        inputOperator(event.target.value)
+    })
+})
+
+equalSign.addEventListener("click",()=>{
+    calculate()
+    updateScreen(currentNumber)
+})
+
+inputDecimal = (dot) => {
+    if(currentNumber.includes(".")){
+        return
+    }
+    currentNumber+=dot
+}
+percentage.addEventListener("click", (event)=>{
+    calculationOperator = "percentage"
+})
+decimal.addEventListener("click", (event)=>{
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
+const calculate=()=>{
+    let result=""
+    switch (calculationOperator){
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+        break
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber)
+        break
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber)
+        break
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber)
+        break
+        case "percentage":
+            result = parseFloat(currentNumber) / 100
+        break
+        default:
+        break
+    }
+    currentNumber = result
+    calculationOperator = ""
+}
+const clearAll = ()=>{
+    prevNumber = ''
+    currentNumber = '0'
+    calculationOperator = ''
+
+}
+clearBtn.addEventListener("click",()=>{
+    clearAll()
+    updateScreen(currentNumber)
+})
